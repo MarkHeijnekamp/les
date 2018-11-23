d91d119836a685991c205427a0f126fccf470a82
Mark Heijnekamp <26150@ma-web.nl>
2018-11-23 17:17:06 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mike nieuweMike = new Mike("ASS");
+        Mike THIJS = new Mike("MORE ASSS");
+        nieuweMike.kleur = "Groen";
+        THIJS.Cadautjegeven();
+
+        System.out.println(nieuweMike.kleur);
+
+    }
+}
---
+++ src/com/company/Mike.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Mike
+{
+    public String kleur = "BLAUW";
+    public String cadeau = "ACTION MAN";
+
+    public Mike(String Nieuwcadeau){
+        cadeau = Nieuwcadeau;
+    }
+    public void Cadautjegeven()
+    {
+        System.out.println(cadeau );
+    }
+
+}
